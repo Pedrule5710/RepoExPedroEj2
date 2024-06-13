5f1ebf2600aac6cd9a41de9e6e5e70ee4714ef3e
Pedrule5710 <m.r.pedrule@gmail.com>
2024-06-13 10:05:43 +0200

    creamos funcionalidad clase analisisEdad

--- src/AnalisisEdad.java
+++ src/AnalisisEdad.java
@@ -1,4 +1,30 @@
+import java.util.*;
 
 public class AnalisisEdad {
+	private ArrayList<Integer> edades = new ArrayList<Integer>();
+	public AnalisisEdad(ArrayList<Integer> edades) {
+		edades.add(15);
+		edades.add(20);
+		edades.add(12);
+		edades.add(13);
+		edades.add(9);
+		edades.add(11);
+
+	}
+	
+	public String comprobar(ArrayList<Integer> edades) {
+		int suma=0, cuenta=0;
+		for(int i =0; i<edades.size();i++) {
+			if(edades.get(i)%2 == 0){
+				suma=suma + edades.get(i);
+			}else if(edades.get(i)%2 != 0) {
+				cuenta = cuenta + 1;
+			}
+		}
+		System.out.println("Suma de pares: " + suma + " ,recuento de impares: " + cuenta);
+		
+		
+		return ("Suma de pares: " + suma + " ,recuento de impares: " + cuenta);
+	}
 
 }
